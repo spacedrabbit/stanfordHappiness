4d2a86258994942c923f26af0b4cef125342eeda
Louis Tur <louis.tur@gmail.com>
2015-09-10 22:06:00 -0400

    Initial commit, covers code from lecture 5

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Louis Tur on 9/10/15.
+//  Copyright (c) 2015 Louis Tur. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+
+
+}
